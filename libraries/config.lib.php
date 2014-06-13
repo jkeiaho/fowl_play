a39b91d4c3e744a0ac94325b60fd534c25df9c8e
James Keiaho <jkeiaho@yahoo.com>
2014-06-13 18:28:25 +1200

    added NZ native bird images

---
+++ libraries/config.lib.php
@@ -0,0 +1,10 @@
+<?php
+
+# added to all models in the contruct function. 
+
+class Config{
+	public static $hostname = 'localhost';
+	public static $username = 'davidsai_cmsuser';
+	public static $password = 'cms123';
+	public static $database = 'davidsai_cms';
+}
